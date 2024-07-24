b8d508d443b61b3c2c6658176699807c103617af
kennethlukestorey <kennethlukestorey@gmail.com>
2024-07-24 16:30:16 -0500

    up

--- config/db_neo4j.js
+++
@@ -1,9 +0,0 @@
-const neo4j = require('neo4j-driver');
-
-const db = neo4j.driver(
-  'neo4j://localhost',
-  neo4j.auth.basic('username', 'password')
-);
-const session = driver.session();
-
-module.exports = db;
